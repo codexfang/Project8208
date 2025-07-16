e5e93d324779ecbe2bfcf3fa2e649189f747f6aa
Jason Fang <jasonfang48@gmail.com>
2025-07-16 10:38:05 -0700

    ba

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Project8208/', // MUST match your repo name
   plugins: [react()],
+  base: '/Project8208/' // 👈 add this line
 })

e7de02bef22374f1b6aec72cedb9eb2de6d15fad
Jason Fang <jasonfang48@gmail.com>
2025-07-16 10:30:42 -0700

    Delete college-predictor directory

--- college-predictor/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/Project8208/', // MUST match your repo name
-  plugins: [react()],
-})
